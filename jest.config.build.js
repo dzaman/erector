1fbe29d23e32b02472896458938e2db905caf5a1
Matthew Dzaman <matthew@dzaman.org>
2019-10-10 20:13:33 -0700

    Downversion syntax in jest config

--- jest.config.build.js
+++ jest.config.build.js
@@ -1,7 +1,6 @@
 const base_config = require('./jest.config.js');
 
-module.exports = {
-  ...base_config,
+Object.assign(module.exports, base_config, {
   modulePaths: ['<rootDir>/build'],
   transform: undefined,
-};
+});
