357ea4dee43457cc4bc6bb5c00930eb8322e415c
Matthew Dzaman <matthew@dzaman.org>
2019-10-18 00:19:56 -0700

    start removing isPlainObject

--- lib/util.ts
+++ lib/util.ts
@@ -32,3 +32,53 @@ export const isObject = (value: any) => {
   var type = typeof value;
   return value != null && (type == 'object' || type == 'function');
 }
+
+const getRawTag = (value: any) => {
+  var isOwn = Object.hasOwnProperty.call(value, Symbol.toStringTag),
+      tag = value[Symbol.toStringTag];
+
+  try {
+    value[Symbol.toStringTag] = undefined;
+    var unmasked = true;
+  } catch (e) {}
+
+  var result = Object.prototype.toString.call(value);
+  if (unmasked) {
+    if (isOwn) {
+      value[Symbol.toStringTag] = tag;
+    } else {
+      delete value[Symbol.toStringTag];
+    }
+  }
+  return result;
+}
+
+const baseGetTag = (value: any) => {
+  if (value === undefined) {
+    return '[object Undefined]';
+  } else if (value === null) {
+    return '[object Null]';
+  } else {
+    return (Symbol.toStringTag in Object(value)) ? getRawTag(value) : Object.prototype.toString(value);
+  }
+}
+
+export const isPlainObject = (value: any) => {
+  if (value !== null && typeof value === 'object' || baseGetTag(value) !== '[object Object]') {
+    return false;
+  }
+  // function overArg(func, transform) {
+  //   return function(arg) {
+  //     return func(transform(arg));
+  //   };
+  // }
+  // overArg(Object.getPrototypeOf, Object)
+  // handle null/undefined case
+  var proto = Object.getPrototypeOf(Object(value));
+  if (proto === null) {
+    return true;
+  }
+  var Ctor = hasOwnProperty.call(proto, 'constructor') && proto.constructor;
+  return typeof Ctor == 'function' && Ctor instanceof Ctor &&
+    Function.prototype.toString.call(Ctor) == objectCtorString;
+}
